aff38fe1f52c53f26e688b2509509a407b7411c1
Jessica-G-arcia <jeh.garcia.jg@gmail.com>
2024-10-02 10:08:44 -0300

    v2_sobre_nós

--- JS/script.js
+++
@@ -1,5 +0,0 @@
-// Adicione um evento de clique ao botão "Saiba mais"
-document.querySelector('.cartao button').addEventListener('click', function() {
-  // Adicione uma classe para girar o cartão
-  document.querySelector('.cartao').classList.toggle('girar');
-});
